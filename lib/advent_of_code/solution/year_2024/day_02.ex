ea15885493aa3e81927c9187a37483aeeba38aeb
van-sprundel <ramonvansprundel@gmail.com>
2024-12-02 19:21:17 +0100

    Solve day 2 part 1

--- lib/advent_of_code/solution/year_2024/day_02.ex
+++ lib/advent_of_code/solution/year_2024/day_02.ex
@@ -1,5 +1,38 @@
 defmodule AdventOfCode.Solution.Year2024.Day02 do
-  def part1(_input) do
+  def split_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.trim()
+      |> String.split(" ")
+      |> Enum.map(&String.to_integer/1)
+    end)
+  end
+
+  def part1(input) do
+    input
+    |> split_input()
+    |> Enum.filter(fn line ->
+      chunks = Enum.chunk_every(line, 2, 1, :discard)
+
+      # 2 rules
+      # 1. dont change between decr/incr in the same line
+      same_direction =
+        chunks
+        |> Enum.map(fn [a, b] -> a - b end)
+        |> Enum.all?(fn diff -> diff > 0 end) ||
+          Enum.all?(chunks, fn [a, b] -> b - a > 0 end)
+
+      # 2. difference has to be range 1..3
+      valid_differences =
+        Enum.all?(chunks, fn [a, b] ->
+          abs(a - b) in 1..3
+        end)
+
+      same_direction && valid_differences
+    end)
+    |> Enum.count()
   end
 
   def part2(_input) do
