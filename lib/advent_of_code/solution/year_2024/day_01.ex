99c5ed2e37731c42f5ff8c0e055dfb9fd03ba2e0
van-sprundel <ramonvansprundel@gmail.com>
2024-12-02 18:20:39 +0100

    Solve day 1 part 1

--- lib/advent_of_code/solution/year_2024/day_01.ex
+++ lib/advent_of_code/solution/year_2024/day_01.ex
@@ -1,5 +1,24 @@
 defmodule AdventOfCode.Solution.Year2024.Day01 do
-  def part1(_input) do
+  def parse_text(text) do
+    text
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line |> String.split(~r/\s{3}/) |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.reduce({[], []}, fn [l, r], {left, right} -> {[l | left], [r | right]} end)
+  end
+
+  def total_distance(left, right) do
+    left
+    |> Enum.sort()
+    |> Enum.zip(Enum.sort(right))
+    |> Enum.reduce(0, fn {l, r}, acc -> acc + abs(l - r) end)
+  end
+
+  def part1(input) do
+    input
+    |> parse_text()
+    |> (fn {left, right} -> total_distance(left, right) end).()
   end
 
   def part2(_input) do
--- test/advent_of_code/solution/year_2024/day_01_test.exs
+++ test/advent_of_code/solution/year_2024/day_01_test.exs
@@ -6,15 +6,20 @@ defmodule AdventOfCode.Solution.Year2024.Day01Test do
   setup do
     [
       input: """
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
   @tag :skip
