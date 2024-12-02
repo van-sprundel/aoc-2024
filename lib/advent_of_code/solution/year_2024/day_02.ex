9cc20959d4a5d2cf7f5718e5284a801bff3bfdc2
van-sprundel <ramonvansprundel@gmail.com>
2024-12-02 19:28:03 +0100

    Cleanup, add comments

--- lib/advent_of_code/solution/year_2024/day_02.ex
+++ lib/advent_of_code/solution/year_2024/day_02.ex
@@ -17,40 +17,32 @@ defmodule AdventOfCode.Solution.Year2024.Day02 do
       chunks = Enum.chunk_every(line, 2, 1, :discard)
 
       # 2 rules
-      # 1. dont change between decr/incr in the same line
-      same_direction =
-        chunks
-        |> Enum.map(fn [a, b] -> a - b end)
-        |> Enum.all?(fn diff -> diff > 0 end) ||
-          Enum.all?(chunks, fn [a, b] -> b - a > 0 end)
+      # 1. don't change direction in between lines
+      increasing = Enum.all?(chunks, fn [a, b] -> b - a > 0 end)
+      decreasing = Enum.all?(chunks, fn [a, b] -> a - b > 0 end)
 
-      # 2. difference has to be range 1..3
-      valid_differences =
-        Enum.all?(chunks, fn [a, b] ->
-          abs(a - b) in 1..3
-        end)
+      # 2. difference between each chunk is range 1..3
+      valid_diff = Enum.all?(chunks, fn [a, b] -> abs(a - b) in 1..3 end)
 
-      same_direction && valid_differences
+      (increasing || decreasing) && valid_diff
     end)
     |> Enum.count()
   end
 
+  # same for part2, but if removing max 1 element fixes it then it's also valid
   def part2(input) do
     input
     |> split_input()
     |> Enum.filter(fn line ->
-      # Helper function to check if a sequence is valid
-      valid_sequence = fn sequence ->
-        Enum.all?(sequence, fn [a, b] -> abs(a - b) in 1..3 end) &&
-          (Enum.all?(sequence, fn [a, b] -> a - b > 0 end) ||
-             Enum.all?(sequence, fn [a, b] -> b - a > 0 end))
-      end
-
-      # allow at most one mistake by removing one element
       Enum.any?(0..(length(line) - 1), fn skip_index ->
         modified_line = List.delete_at(line, skip_index)
-        modified_chunks = Enum.chunk_every(modified_line, 2, 1, :discard)
-        valid_sequence.(modified_chunks)
+        chunks = Enum.chunk_every(modified_line, 2, 1, :discard)
+
+        increasing = Enum.all?(chunks, fn [a, b] -> b - a > 0 end)
+        decreasing = Enum.all?(chunks, fn [a, b] -> a - b > 0 end)
+        valid_diff = Enum.all?(chunks, fn [a, b] -> abs(a - b) in 1..3 end)
+
+        (increasing || decreasing) && valid_diff
       end)
     end)
     |> Enum.count()
